62a379b18679808bae2a724cf50d99bb4a801cc1
Luis Martinez <luis_amc@unitec.edu>
2021-07-26 16:51:26 -0600

    init + tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,26 @@
+const colors = require('tailwindcss/colors');
+
+const brandColor = colors.blue;
+
 module.exports = {
   mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./src/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: colors.gray,
+        // Add a new "brand" color to all Tailwind utilities, so that we can easily change it.
+        brand: brandColor
+      },
+      // Modify the default ring color so that it matches the brand color:
+      ringColor: {
+        DEFAULT: brandColor['500']
+      }
+    }
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
-}
+  plugins: []
+};
