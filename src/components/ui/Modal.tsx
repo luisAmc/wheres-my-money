56128a7b82ace6bb17216719c66bfacd410aebf4
Luis Martinez <luis_amc@unitec.edu>
2021-07-31 20:03:24 -0600

    Add modal

---
+++ src/components/ui/Modal.tsx
@@ -0,0 +1,91 @@
+import { Dialog, Transition } from '@headlessui/react';
+import { XIcon } from '@heroicons/react/outline';
+import { useState } from 'react';
+import { Fragment, ReactNode } from 'react';
+
+export function useModal() {
+  const [open, setOpen] = useState(false);
+
+  return {
+    open: () => setOpen(true),
+    close: () => setOpen(false),
+    props: {
+      open,
+      onClose() {
+        setOpen(false);
+      }
+    }
+  };
+}
+
+interface Props {
+  title: string;
+  open: boolean;
+  onClose: () => void;
+  children: ReactNode;
+}
+
+export function Modal({ title, open, onClose, children }: Props) {
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog
+        as='div'
+        static
+        className='fixed inset-0 z-10 overflow-y-auto'
+        open={open}
+        onClose={onClose}
+      >
+        <div className='flex items-end justify-center min-h-screen px-4 text-center sm:block'>
+          <Transition.Child
+            as={Fragment}
+            enter='ease-out duration-300'
+            enterFrom='opacity-0'
+            enterTo='opacity-100'
+            leave='ease-in duration-200'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
+          >
+            <Dialog.Overlay className='fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity' />
+          </Transition.Child>
+
+          {/* This element is to trick the browser into centering the modal contents. */}
+          <span
+            className='inline-block h-screen align-middle'
+            aria-hidden='true'
+          >
+            &#8203;
+          </span>
+
+          <Transition.Child
+            as={Fragment}
+            enter='ease-out duration-300'
+            enterFrom='opacity-0 scale-95'
+            enterTo='opacity-100 scale-100'
+            leave='ease-in duration-200'
+            leaveFrom='opacity-100 scale-100'
+            leaveTo='opacity-0 scale-95'
+          >
+            <div className='inline-block w-full max-w-md px-4 mb-6 sm:mb-0 pt-5 pb-4 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-xl'>
+              <div className='flex items-center justify-between mb-3'>
+                <Dialog.Title
+                  as='h3'
+                  className='text-lg leading-6 font-medium text-gray-900'
+                >
+                  {title}
+                </Dialog.Title>
+                <button
+                  type='button'
+                  className='bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none'
+                  onClick={() => onClose()}
+                >
+                  <XIcon className='!w-6 !h-6' />
+                </button>
+              </div>
+              {children}
+            </div>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+}
